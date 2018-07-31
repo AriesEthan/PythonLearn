4497d3a6357bf83392d58849cf7a49f045bd201d
Ethan <yvizhang@gmail.com>
2018-08-01 01:48:15 +0800

    add gitignore and test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import requests
+
+"""response = requests.get("https://www.zhihu.com")
+print(type(response))
+print(response.status_code)
+print(type(response.text))
+print(response.text)
+print(response.cookies)
+print(response.content)
+print(response.content.decode("utf-8"))
+
+response.encoding="utf-8"
+print(response.text)
+"""
+import requests
+import json
+response = requests.get("http://httpbin.org/get")
+print(type(response.text))
+print(response.json())
+print(json.loads(response.text))
+print(type(response.json()))
